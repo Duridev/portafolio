d23c7fb16ce95cbfb6a98cb3d9df22706f234240
Daniel Uribe <duribegalaz@gmail.com>
2023-07-17 00:02:54 -0400

    cambio de imagen

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,15 @@
 const espacio = document.getElementById('espacio');
-const numEstrellas = 800;
+const numEstrellas = 600;
+const alturaSitioWeb = window.innerHeight; // Obtiene la altura del sitio web en unidades de vh
+
+
 
 for (let i = 0; i < numEstrellas; i++) {
   const estrella = document.createElement('div');
   estrella.style.width = '2.5px';
   estrella.style.height = '2.5px';
-  estrella.style.left = Math.random() * 100 + 'vw';
-  estrella.style.top = Math.random() * 400 + 'vh';
+  estrella.style.left = Math.random() * 97 + '%';
+  estrella.style.top = Math.random()  * 300 + '%';
   estrella.className = 'estrella';
   espacio.appendChild(estrella);
 }
